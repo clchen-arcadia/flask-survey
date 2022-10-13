88fedb11321262df07a8398fececd017f72a709f
Jennifer Reuting <jen@reuting.com>
2022-10-13 16:45:32 -0700

    small code review changes

--- app.py
+++ app.py
@@ -19,14 +19,17 @@ def get_landing_page():
     title = survey.title
     instructions = survey.instructions
 
-    return render_template('survey_start.html',
-        title = title,
-        instructions = instructions
+    return render_template(
+        'survey_start.html',
+        title=title,
+        instructions=instructions
     )
+
 
 @app.post('/begin')
 def handle_start_btn():
     """Handles survey start button"""
+
     responses.clear()
 
     return redirect("/question/0")
@@ -35,20 +38,25 @@ def handle_start_btn():
 @app.post('/response/<int:q_idx>')
 def handle_survey_continuation(q_idx):
     """Handles next question continuation"""
+
     response = request.form.get("response")
 
     responses.append(response)
-    print("responses ------------------>", responses)
 
     q_idx += 1
 
-    if(q_idx < len(survey.questions)):
+    if (q_idx < len(survey.questions)):
         return redirect(f'/question/{q_idx}')
     else:
-        qa_tuples = [(p1.question, p2) for idx1, p1 in enumerate(survey.questions)
-        for idx2, p2 in enumerate(responses) if idx1 == idx2]
-        
-        return render_template('/completion.html', qa_tuples = qa_tuples)
+        qa_tuples = [  # TODO - name better
+            (p1.question, p2)
+            for idx1, p1 in enumerate(survey.questions)
+            for idx2, p2 in enumerate(responses)
+            if idx1 == idx2
+        ]
+
+        return render_template('/completion.html', qa_tuples=qa_tuples)
+
 
 @app.get('/question/<int:q_idx>')
 def load_questions_page(q_idx):
@@ -57,10 +65,9 @@ def load_questions_page(q_idx):
     question = survey.questions[q_idx]
     choices = question.choices
 
-
-    return render_template('question.html',
-        question = question,
-        choices = choices,
-        q_idx = q_idx
+    return render_template(
+        'question.html',
+        question=question,
+        choices=choices,
+        q_idx=q_idx
     )
-
